2a44e149921031a7c356d26f3297c593aace8af7
siddhantvyas-tech <mailsiddhantvyas@gmail.com>
2021-01-04 15:26:27 +0530

    Student model updates

--- src/main/java/com/example/model/Student.java
+++ src/main/java/com/example/model/Student.java
@@ -1,4 +1,76 @@
 package com.example.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import java.util.UUID;
+
+@Entity
 public class Student {
+
+    @Id
+    @GeneratedValue
+    private UUID studentId;
+    private String name;
+    private String contact;
+    private String email;
+    private String password;
+    private String address;
+    private String marks;
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getMarks() {
+        return marks;
+    }
+
+    public void setMarks(String marks) {
+        this.marks = marks;
+    }
+
+    public UUID getStudentId() {
+        return studentId;
+    }
+
+    public void setStudentId(UUID studentId) {
+        this.studentId = studentId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getContact() {
+        return contact;
+    }
+
+    public void setContact(String contact) {
+        this.contact = contact;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
